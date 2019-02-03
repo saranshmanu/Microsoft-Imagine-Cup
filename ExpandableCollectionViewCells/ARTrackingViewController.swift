6519cb15009318908bf3d417f5beb4113c5979a0
Saransh Mittal <saranshmanu@yahoo.co.in>
2019-02-03 23:36:33 +0530

    Created the nodes for ar in a customised manner

--- ExpandableCollectionViewCells/ARTrackingViewController.swift
+++ ExpandableCollectionViewCells/ARTrackingViewController.swift
@@ -11,31 +11,27 @@ import SceneKit
 import ARKit
 import Vision
 
+@available(iOS 12.0, *)
 class ARTrackingViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var debugTextView: UITextView!
     
-    var NODES = [SCNNode]()
-    var closestRESULT = [ARHitTestResult]()
+    var Nodes = [SCNNode]()
+    var closestResults = [ARHitTestResult]()
     var detectedObjectCode = [String]()
-    let bubbleDepth : Float = 0.01 // the 'depth' of 3D text
-    var latestPrediction : String = "…" // a variable containing the latest CoreML prediction
-    var visionRequests = [VNRequest]()
     var totalLength = 0
-    let dispatchQueueML = DispatchQueue(label: "com.hw.dispatchqueueml") // A Serial Queue
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-    
-    // MARK: - ARSCNViewDelegate
-    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-        DispatchQueue.main.async {
-            // Do any desired updates to SceneKit here.
-        }
+    func changeNodeText(text:String, number:Int){
+        Nodes[number].removeFromParentNode()
+        Nodes.remove(at: number)
+        var closestResult = closestResults[number]
+        let transform : matrix_float4x4 = closestResult.worldTransform
+        let worldCoord : SCNVector3 = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+        let node : SCNNode = createNewChildNode(text)
+        sceneView.scene.rootNode.addChildNode(node)
+        node.position = worldCoord
+        Nodes.insert(node, at: number)
     }
     
     func selectedPinNode(number:Int, filter:String) {
@@ -44,18 +40,6 @@ class ARTrackingViewController: UIViewController, ARSCNViewDelegate {
     
     func deselectPinNode(number:Int, filter:String) {
         changeNodeText(text: filter, number: number)
-    }
-    
-    func changeNodeText(text:String, number:Int){
-        NODES[number].removeFromParentNode()
-        NODES.remove(at: number)
-        var closestResult = closestRESULT[number]
-        let transform : matrix_float4x4 = closestResult.worldTransform
-        let worldCoord : SCNVector3 = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
-        let node : SCNNode = createNewBubbleParentNode(text)
-        sceneView.scene.rootNode.addChildNode(node)
-        node.position = worldCoord
-        NODES.insert(node, at: number)
     }
     
     override func viewDidLoad() {
@@ -97,53 +81,111 @@ class ARTrackingViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Pause the view's session
+//        sceneView.session.pause()
+    }
+    
+    let dispatchQueueML = DispatchQueue(label: "com.hw.dispatchqueueml") // A Serial Queue
+    var visionRequests = [VNRequest]()
+    var latestPrediction : String = "…" // a variable containing the latest CoreML prediction
+    
+    // MARK: - ARSCNViewDelegate
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        DispatchQueue.main.async {
+            // Do any desired updates to SceneKit here.
+        }
+    }
+    
     @objc func handleTap(gestureRecognize: UITapGestureRecognizer) {
         // HIT TEST : REAL WORLD
+        
         // Get Screen Centre
-        let screenCentre : CGPoint = CGPoint(x: self.sceneView.bounds.midX, y: self.sceneView.bounds.midY)
-        let arHitTestResults : [ARHitTestResult] = sceneView.hitTest(screenCentre, types: [.featurePoint]) // Alternatively, we could use '.existingPlaneUsingExtent' for more grounded hit-test-points.
-        //        print(arHitTestResults)
+        let screenCentre:CGPoint = CGPoint(x: self.sceneView.bounds.midX, y: self.sceneView.bounds.midY)
+        let arHitTestResults:[ARHitTestResult] = sceneView.hitTest(screenCentre, types: [.featurePoint]) // Alternatively, we could use '.existingPlaneUsingExtent' for more grounded hit-test-points.
         if let closestResult = arHitTestResults.first {
             // Get Coordinates of HitTest
-            let transform : matrix_float4x4 = closestResult.worldTransform
-            let worldCoord : SCNVector3 = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+            let transform:matrix_float4x4 = closestResult.worldTransform
+            let worldCoord:SCNVector3 = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
             // Create 3D Text
-            let node : SCNNode = createNewBubbleParentNode(latestPrediction)
+            let node:SCNNode = createNewChildNode(latestPrediction)
             sceneView.scene.rootNode.addChildNode(node)
             node.position = worldCoord
-            NODES.append(node)
-            detectedObjectCode.append(latestPrediction)
-            closestRESULT.append(closestResult)
-            totalLength = NODES.count
+//            Nodes.append(node)
+//            detectedObjectCode.append(latestPrediction)
+//            closestResults.append(closestResult)
+//            totalLength = Nodes.count
         }
     }
     
     
-    func createNewBubbleParentNode(_ text : String) -> SCNNode {
-        let billboardConstraint = SCNBillboardConstraint()
-        billboardConstraint.freeAxes = SCNBillboardAxis.Y
-        let bubble = SCNText(string: text, extrusionDepth: CGFloat(bubbleDepth))
+    func createNewChildNode(_ text : String) -> SCNNode {
+        let prediction = text
+        let textDepth : Float = 0.01
+        let text = SCNText(string: text, extrusionDepth: CGFloat(textDepth))
         let font = UIFont(name: "Arial", size: 0.2)
-        bubble.font = font
-        bubble.alignmentMode = kCAAlignmentCenter
-        bubble.firstMaterial?.diffuse.contents = UIColor.white
-        bubble.firstMaterial?.specular.contents = UIColor.white
-        bubble.firstMaterial?.isDoubleSided = true
-        bubble.chamferRadius = CGFloat(bubbleDepth)
-        let (minBound, maxBound) = bubble.boundingBox
+        text.font = font
+        text.alignmentMode = kCAAlignmentCenter
+        text.firstMaterial?.diffuse.contents = UIColor.white
+        text.firstMaterial?.specular.contents = UIColor.white
+        text.firstMaterial?.isDoubleSided = true
+        text.chamferRadius = CGFloat(textDepth)
+        let (minBound, maxBound) = text.boundingBox
+        let textNode = SCNNode(geometry: text)
+        textNode.pivot = SCNMatrix4MakeTranslation( (maxBound.x - minBound.x)/2, minBound.y, textDepth/2)
+        textNode.scale = SCNVector3Make(0.2, 0.2, -0.2)
         
-        let bubbleNode = SCNNode(geometry: bubble)
-        bubbleNode.pivot = SCNMatrix4MakeTranslation( (maxBound.x - minBound.x)/2, minBound.y, bubbleDepth/2)
-        bubbleNode.scale = SCNVector3Make(0.2, 0.2, -0.2)
-        let sphere = SCNSphere(radius: 0.004)
+        
+        let sphere = SCNSphere(radius: 0.002)
         sphere.firstMaterial?.diffuse.contents = UIColor.cyan
         let sphereNode = SCNNode(geometry: sphere)
+//        sphereNode.scale = SCNVector3Make(0.2, 0.2, 0.2)
+        sphereNode.position = SCNVector3Make(0, 0, 0)
         
-        let bubbleNodeParent = SCNNode()
-        bubbleNodeParent.addChildNode(bubbleNode)
-        bubbleNodeParent.addChildNode(sphereNode)
-        bubbleNodeParent.constraints = [billboardConstraint]
-        return bubbleNodeParent
+        let plane = SCNPlane(width: CGFloat(0.2), height: CGFloat(0.1))
+        plane.cornerRadius = plane.width
+        let spriteKitScene = SKScene(fileNamed: "ProductInformation")
+        spriteKitScene?.backgroundColor = UIColor.init(red: 1, green: 1, blue: 1, alpha: 0.3)
+        let image = SKSpriteNode(imageNamed: "Avatar")
+        let label = SKLabelNode(fontNamed: "Arial")
+        label.text = prediction
+        label.fontSize = 60
+        spriteKitScene?.addChild(label)
+//        spriteKitScene?.addChild(image)
+        plane.firstMaterial?.diffuse.contents = spriteKitScene
+        plane.firstMaterial?.isDoubleSided = false
+        plane.firstMaterial?.diffuse.contentsTransform = SCNMatrix4Translate(SCNMatrix4MakeScale(1, -1, 1), 0, 1, 0)
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.position = SCNVector3Make(0, 0.1, 0)
+        
+        let plane2 = SCNPlane(width: CGFloat(0.2), height: CGFloat(0.1))
+        plane2.cornerRadius = plane2.width
+        let spriteKitScene2 = SKScene(fileNamed: "ProductInformation")
+        spriteKitScene2?.backgroundColor = UIColor.init(red: 1, green: 1, blue: 1, alpha: 0.3)
+        let image2 = SKSpriteNode(imageNamed: "Avatar")
+        image2.size.height = 10
+//        let label2 = SKLabelNode(fontNamed: "Arial")
+//        label2.text = prediction
+//        label2.fontSize = 60
+//        spriteKitScene2?.addChild(label2)
+        spriteKitScene2?.addChild(image2)
+        plane2.firstMaterial?.diffuse.contents = spriteKitScene2
+        plane2.firstMaterial?.isDoubleSided = false
+        plane2.firstMaterial?.diffuse.contentsTransform = SCNMatrix4Translate(SCNMatrix4MakeScale(1, -1, 1), 0, 1, 0)
+        let planeNode2 = SCNNode(geometry: plane2)
+        planeNode2.position = SCNVector3Make(0, 0.23, 0)
+        
+        
+        let billboardConstraint = SCNBillboardConstraint()
+        billboardConstraint.freeAxes = SCNBillboardAxis.Y
+        let childParent = SCNNode()
+        childParent.addChildNode(sphereNode)
+        childParent.addChildNode(planeNode2)
+//        childParent.addChildNode(textNode)
+        childParent.addChildNode(planeNode)
+        childParent.constraints = [billboardConstraint]
+        return childParent
     }
     
     func loopCoreMLUpdate() {
@@ -166,14 +208,11 @@ class ARTrackingViewController: UIViewController, ARSCNViewDelegate {
             print("No results")
             return
         }
-        
         // Get Classifications
         let classifications = observations[0...1] // top 2 results
             .flatMap({ $0 as? VNClassificationObservation })
             .map({ "\($0.identifier) \(String(format:"- %.2f", $0.confidence))" })
             .joined(separator: "\n")
-        
-        
         DispatchQueue.main.async {
             // Print Classifications
             //            print(classifications)
@@ -183,7 +222,6 @@ class ARTrackingViewController: UIViewController, ARSCNViewDelegate {
             var debugText:String = ""
             debugText += classifications
             self.debugTextView.text = debugText
-            
             // Store the latest prediction
             var objectName:String = "…"
             objectName = classifications.components(separatedBy: "-")[0]
@@ -194,20 +232,15 @@ class ARTrackingViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func updateCoreML() {
-        ///////////////////////////
         // Get Camera Image as RGB
         let pixbuff : CVPixelBuffer? = (sceneView.session.currentFrame?.capturedImage)
         if pixbuff == nil { return }
         let ciImage = CIImage(cvPixelBuffer: pixbuff!)
         // Note: Not entirely sure if the ciImage is being interpreted as RGB, but for now it works with the Inception model.
         // Note2: Also uncertain if the pixelBuffer should be rotated before handing off to Vision (VNImageRequestHandler) - regardless, for now, it still works well with the Inception model.
-        
-        ///////////////////////////
         // Prepare CoreML/Vision Request
         let imageRequestHandler = VNImageRequestHandler(ciImage: ciImage, options: [:])
         // let imageRequestHandler = VNImageRequestHandler(cgImage: cgImage!, orientation: myOrientation, options: [:]) // Alternatively; we can convert the above to an RGB CGImage and use that. Also UIInterfaceOrientation can inform orientation values.
-        
-        ///////////////////////////
         // Run Image Request
         do {
             try imageRequestHandler.perform(self.visionRequests)
@@ -216,15 +249,4 @@ class ARTrackingViewController: UIViewController, ARSCNViewDelegate {
         }
         
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
