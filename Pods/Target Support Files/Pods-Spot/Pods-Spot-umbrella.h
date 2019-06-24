92eadbcdc01af6c7d1bdde6e9b7ff1c448f3b0dc
Saransh Mittal <saranshmanu@yahoo.co.in>
2019-06-25 00:22:36 +0530

    Upgrading the project to MVC

--- Pods/Target Support Files/Pods-Spot/Pods-Spot-umbrella.h
+++ Pods/Target Support Files/Pods-Spot/Pods-Spot-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_ExpandableCollectionViewCellsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ExpandableCollectionViewCellsVersionString[];
+FOUNDATION_EXPORT double Pods_SpotVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SpotVersionString[];
 
